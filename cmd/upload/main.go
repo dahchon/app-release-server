c3fc15b4467d2b186bbf91646edc4d274ff00c4c
dahchon <daohanchong@gmail.com>
2023-08-26 09:30:02 -0700

    initial commit

---
+++ cmd/upload/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+type AppDetails struct {
+	AppVersion string `json:"app_version"`
+	AppBuild   string `json:"app_build"`
+	AppName    string `json:"app_name"`
+	GitCommit  string `json:"git_commit,omitempty"`
+}
+
+func main() {
+	// Define flags
+	filePath := flag.String("file", "", "Path to the file being uploaded")
+	backendURL := flag.String("url", "http://localhost:8080/admin/upload/app", "Backend URL")
+	appName := flag.String("name", "", "Application name")
+	appBuild := flag.String("build", "", "Application build")
+	appVersion := flag.String("version", "", "Application version")
+	gitCommit := flag.String("commit", "", "Git commit")
+
+	// Parse flags
+	flag.Parse()
+
+	if *filePath == "" {
+		fmt.Println("You must provide a file path")
+		return
+	}
+
+	if *appName == "" || *appBuild == "" || *appVersion == "" {
+		fmt.Println("You must provide the application name, build, and version")
+		return
+	}
+
+	// Open the file
+	file, err := os.Open(*filePath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	// Create a buffer to store our request body
+	body := &bytes.Buffer{}
+
+	// Create a multipart writer
+	writer := multipart.NewWriter(body)
+
+	// Create a form field for the file
+	fileWriter, err := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Copy the file into the fileWriter
+	_, err = io.Copy(fileWriter, file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Prepare the details
+	details := AppDetails{
+		AppName:    *appName,
+		AppBuild:   *appBuild,
+		AppVersion: *appVersion,
+		GitCommit:  *gitCommit,
+	}
+	detailsJSON, err := json.Marshal(details)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Add the other fields
+	_ = writer.WriteField("details", string(detailsJSON))
+
+	// Close the multipart writer
+	if err := writer.Close(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Create a new request
+	req, err := http.NewRequest("POST", *backendURL, body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Set the content type, this is very important
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	// Add basic authentication
+	req.SetBasicAuth("admin", "password") // replace with your own user and password
+
+	// Do the request
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	// Check the response
+	if res.StatusCode == http.StatusOK {
+		fmt.Println("File uploaded successfully")
+	} else {
+		fmt.Println("Failed to upload file")
+	}
+}
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AppDetails struct {
+	AppVersion string `json:"app_version"`
+	AppBuild   string `json:"app_build"`
+	AppName    string `json:"app_name"`
+	GitCommit  string `json:"git_commit,omitempty"`
+}
+
+func main() {
+	r := gin.Default()
+
+	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+		"admin": "password", // replace with your own user and password
+	}))
+
+	authorized.POST("/admin/upload/app", func(c *gin.Context) {
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var json AppDetails
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		dir := fmt.Sprintf("./apps/%s/%s/%s", json.AppName, json.AppVersion, json.AppBuild)
+		err = os.MkdirAll(dir, os.ModePerm)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		dst := fmt.Sprintf("%s/binary.app", dir)
+		if err := c.SaveUploadedFile(file, dst); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "File uploaded successfully"})
+	})
+
+	r.GET("/apps/:app_name/:app_version/:app_build/binary.app", func(c *gin.Context) {
+		appName := c.Param("app_name")
+		appVersion := c.Param("app_version")
+		appBuild := c.Param("app_build")
+
+		if appName == "" || appVersion == "" || appBuild == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Missing app name, version or build"})
+			return
+		}
+
+		filePath := fmt.Sprintf("./apps/%s/%s/%s/binary.app", appName, appVersion, appBuild)
+		c.File(filePath)
+	})
+
+	r.Run(":8080")
+}
