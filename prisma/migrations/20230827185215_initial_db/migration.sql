284bacd13983492bb51df83d53438213e1a4cec6
dahchon <daohanchong@gmail.com>
2023-08-27 11:52:35 -0700

    reformat db

--- prisma/migrations/20230827185215_initial_db/migration.sql
+++ prisma/migrations/20230827185215_initial_db/migration.sql
@@ -4,16 +4,18 @@ CREATE TABLE "app_releases" (
     "appName" TEXT NOT NULL,
     "appVersion" TEXT NOT NULL,
     "appBuild" TEXT NOT NULL,
+    "target" TEXT NOT NULL,
+    "arch" TEXT NOT NULL,
     "gitCommit" TEXT NOT NULL,
     "mainFileName" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
     "title" TEXT,
     "published" BOOLEAN NOT NULL DEFAULT true,
-    "description" TEXT,
+    "releaseNotes" TEXT,
     "downloadCount" INTEGER NOT NULL DEFAULT 0,
     "uploaderIP" TEXT
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "app_releases_appName_appVersion_appBuild_key" ON "app_releases"("appName", "appVersion", "appBuild");
+CREATE UNIQUE INDEX "app_releases_appName_appVersion_appBuild_target_arch_key" ON "app_releases"("appName", "appVersion", "appBuild", "target", "arch");
